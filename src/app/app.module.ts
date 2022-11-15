1e6406b69438bbcb6267438edccbd31f22e204e0
Benngy <benjistaman.12@gmail.com>
2022-11-15 14:10:11 -0700

    Added alyle ui components and modules for easy ui development. Further developed home screen component, added css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,63 @@
+// Angular imports
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import {
+  BrowserModule,
+  HAMMER_GESTURE_CONFIG,
+  HammerModule,
+} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Alyle imports
+import {
+  LyHammerGestureConfig,
+  LY_THEME,
+  LY_THEME_NAME,
+  StyleRenderer,
+  LyTheme2,
+} from '@alyle/ui';
+import {
+  MinimaLight,
+  MinimaDeepDark,
+  MinimaDark,
+} from '@alyle/ui/themes/minima';
+import { LyCommonModule } from '@alyle/ui';
+import { LyCardModule } from '@alyle/ui/card';
+import { LyTypographyModule } from '@alyle/ui/typography';
+import { LyButtonModule } from '@alyle/ui/button';
+import { LyIconModule } from '@alyle/ui/icon';
+import { LyGridModule } from '@alyle/ui/grid';
+import { LyListModule } from '@alyle/ui/list';
+
+// App imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { homeScreenComponent } from './components/homeScreen/homeScreen.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    homeScreenComponent
-  ],
+  declarations: [AppComponent, homeScreenComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HammerModule,
+    LyCommonModule,
+    LyCardModule,
+    LyTypographyModule,
+    LyButtonModule,
+    LyIconModule,
+    LyListModule,
+    LyGridModule,
   ],
-  providers: [],
-  bootstrap: [homeScreenComponent]
+  providers: [
+    { provide: HAMMER_GESTURE_CONFIG, useClass: LyHammerGestureConfig },
+    StyleRenderer,
+    LyTheme2,
+    { provide: LY_THEME_NAME, useValue: 'minima-light' },
+    { provide: LY_THEME, useClass: MinimaLight, multi: true },
+    { provide: LY_THEME, useClass: MinimaDeepDark, multi: true },
+    { provide: LY_THEME, useClass: MinimaDark, multi: true },
+  ],
+  bootstrap: [homeScreenComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/homeScreen/homeScreen.component.ts
+++ src/app/components/homeScreen/homeScreen.component.ts
@@ -1,11 +1,40 @@
-import { Component } from "@angular/core";
-import { npcClass } from "src/app/models";
+import { Component } from '@angular/core';
+import { npcClass } from 'src/app/models';
+import { ThemeVariables, ThemeRef, lyl, StyleRenderer } from '@alyle/ui';
+
+const STYLES = (theme: ThemeVariables, ref: ThemeRef) => {
+  const __ = ref.selectorsOf(STYLES);
+  return {
+    $global: lyl`{
+      body {
+        background-color: ${theme.background.default}
+        color: ${theme.text.primary}
+        font-family: ${theme.typography.fontFamily}
+        margin: 20
+        direction: ${theme.direction}
+      }
+    }`,
+    root: lyl`{
+      display: block
+    }`,
+    card: lyl `{
+        min-width: 320px
+        max-width: 320px
+        min-height: 500px
+    }`
+  };
+};
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './homeScreen.component.html',
-    styleUrls: []
+  selector: 'app-root',
+  templateUrl: './homeScreen.component.html',
+  styleUrls: ['./homeScreen.component.css'],
+  providers: [StyleRenderer],
 })
 export class homeScreenComponent {
-    title = 'EasyNPCHome'
+  readonly classes = this.sRenderer.renderSheet(STYLES, true);
+
+  title = 'EasyNPCHome';
+
+  constructor(readonly sRenderer: StyleRenderer) {}
 }
